df262d1b17cfd0f331c6fba35e2325b899007bf2
Deyvant R_K_Bhardwaj <58858873+DeyvWeb@users.noreply.github.com>
2022-02-08 23:57:05 +0530

    Add files via upload

---
+++ HarryEx11.java
@@ -0,0 +1,66 @@
+// Create a class circle and use inheritance to create another class cylinder from it
+
+// What is the order of constructor execution for the following inheritance hierarchy 
+//                     Base 
+                                
+//                    Derived 1
+                                    
+//                    Derived 2
+
+// Derived obj = new Derived 2( ); 
+// Which constructor(s) will be executed & in what order?
+
+
+// class Circle{
+    
+//     public int radius;
+
+//     Circle(){
+//         System.out.println("I am non paramterized constructor");
+//     }
+    
+//     Circle(int r){
+//         this.radius = r;
+//         System.out.println("I am circle paramterized constructor");
+//     }
+//     public double area(){
+
+//         return Math.PI*this.radius*this.radius;
+//     }
+// }
+
+// class Cylinder extends Circle{
+//     public int height;
+//     Cylinder(int r, int h){
+//         super(r);
+//         this.height = h;
+//     }
+   
+//     public double volume(){
+//         return Math.PI*this.radius*this.radius*this.radius*this.height;
+//     }
+// }
+
+
+
+
+// public class HarryEx11 {
+// public static void main(String[] args) {
+//     //Circle objc = new Circle(12);
+
+//     Cylinder obj = new Cylinder(12, 10);
+
+//     System.out.print(obj.Circle());
+
+// }    
+// }
+
+
+
+
+// Create a class rectangle and use inheritance to create another class cuboid, try to keep it as close to the real-world scenario as possible 
+// Create a method for area and volume in 1 
+
+// create methods for area & volume in 2 also create getters and setters 
+
+
